248b39303dcca3bd4d235a43c1fcd9f83e41c33a
annakarpf <anakarpf6@gmail.com>
2018-04-14 19:29:00 +0300

    914484

--- 09_Angular/Day 05 - 13.04.2018/01_final-app/README.md
+++
@@ -1,5 +0,0 @@
-## to run the app follow this steps:
-* npm install
-* node app
-
-open the browser in port 3500 AND ENJOY!!
--- 09_Angular/Day 05 - 13.04.2018/01_final-app/app.js
+++
@@ -1,4 +0,0 @@
-const express= require ('express');
-const app=express();
-app.use(express.static('./dist'));
-app.listen(3500,()=>{console.log("server listening on port 3500")});
